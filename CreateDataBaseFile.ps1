adac3403855531d9e0c1c5c49612ea9b8ad58774
Kirill Bezmaternykh <ka.bezmaternykh@gmail.com>
2024-11-27 20:18:39 +0800

    Update CreateDataBaseFile.ps1 to stop execution if file already exists

--- CreateDataBaseFile.ps1
+++ CreateDataBaseFile.ps1
@@ -1,2 +1,12 @@
-sqlite3 tasks.db '.read create-schema.sql'
-sqlite3 tasks.db '.read seed-data.sql'
+$DefaultFileName = "tasks.db"
+$FileName = $args[0]
+if (!$FileName){
+    $FileName = $DefaultFileName
+}
+
+if (Test-Path $FileName){
+    Write-Host "$($FileName) exists. Script terminated"
+    return
+}
+sqlite3 $FileName '.read create-schema.sql'
+sqlite3 $FileName '.read seed-data.sql'
